8c1139c914a2bc6819747bcb2733eda03b18875a
Leonardo Pamplona <lpamplonadev@gmail.com>
2026-01-20 18:31:48 -0300

    feat: add domain exceptions

---
+++ src/main/java/com/lpamplonadev/usermanagement/exception/DuplicateResourceException.java
@@ -0,0 +1,7 @@
+package com.lpamplonadev.usermanagement.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
